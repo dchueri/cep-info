3d887916e25a70229a171899306bcf31cec0bb2b
diego <diegochueri@gmail.com>
2024-06-27 15:41:21 -0300

    feat: add lib

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 const ffi = require("ffi-napi");
+const path = require("path");
 
-const libPath = "./libceps.so";
+const libPath = path.resolve(__dirname, "./libceps.so");
+const filePath = path.resolve(__dirname, "./ceps.csv");
 
-const getNeighborhoodByCEP = ffi.Library(libPath, {
-  get_cep_info: ["string", ["string"]],
+const func = ffi.Library(libPath, {
+  get_cep_info: ["string", ["string", "string"]],
 }).get_cep_info;
+
+const getNeighborhoodByCEP = (cep) => func(cep, filePath);
 
 module.exports = { getNeighborhoodByCEP };
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,9 +4,10 @@ extern crate serde;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::path::Path;
 
 #[no_mangle]
-pub extern "C" fn get_cep_info(cep_ptr: *const i8) -> *const i8 {
+pub extern "C" fn get_cep_info(cep_ptr: *const i8, file_path: *const i8) -> *const i8 {
     let c_str = unsafe {
         assert!(!cep_ptr.is_null());
         std::ffi::CStr::from_ptr(cep_ptr)
@@ -14,7 +15,14 @@ pub extern "C" fn get_cep_info(cep_ptr: *const i8) -> *const i8 {
 
     let str_slice = c_str.to_str().expect("Invalid UTF-8 string");
 
-    if let Ok(file) = File::open("./ceps.csv") {
+    let file_c_str = unsafe {
+        assert!(!file_path.is_null());
+        std::ffi::CStr::from_ptr(file_path)
+    };
+    let file_path_str = file_c_str.to_str().expect("Invalid UTF-8 string");
+    let path = Path::new(file_path_str);
+
+    if let Ok(file) = File::open(path) {
         let reader = BufReader::new(file);
         for line in reader.lines() {
             if let Ok(record) = line {

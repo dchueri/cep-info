ee74b21fed20f450346a39bfe3f783f52859b607
diego <diegochueri@gmail.com>
2024-06-28 11:32:28 -0300

    feat: add more info

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const getNeighborhoodByCEP = (cep) => {
+function getAddressInfoByCEP(cep) {
   try {
     const filePath = path.resolve(__dirname, "./ceps.csv");
     const data = fs.readFileSync(filePath, "utf8");
@@ -11,7 +11,12 @@ const getNeighborhoodByCEP = (cep) => {
       const fields = line.split(";");
 
       if (fields.length >= 4 && fields[0] === cep.replace("-", "")) {
-        return fields[3];
+        return {
+          state: fields[1],
+          city: fields[2],
+          neighborhood: fields[3],
+          street: fields[4].replace("\r", ""),
+        };
       }
     }
 
@@ -20,6 +25,6 @@ const getNeighborhoodByCEP = (cep) => {
     console.error("Error reading file:", error);
     return "FILE_NOT_FOUND";
   }
-};
+}
 
-module.exports = { getNeighborhoodByCEP };
+module.exports = { getAddressInfoByCEP };
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const ffi = require('ffi-napi');
+const fs = require("fs");
+const path = require("path");
 
-const libPath = './libceps.so';
+function getAddressInfoByCEP(cep) {
+  try {
+    const filePath = path.resolve(__dirname, "./ceps.csv");
+    const data = fs.readFileSync(filePath, "utf8");
+    const lines = data.split("\n");
 
-const getNeighborhoodByCEP = ffi.Library(libPath, {
-  'get_cep_info': ['string', ['string']]
-}).get_cep_info;
+    for (let line of lines) {
+      const fields = line.split(";");
 
-export { getNeighborhoodByCEP };
+      if (fields.length >= 4 && fields[0] === cep.replace("-", "")) {
+        return {
+          state: fields[1],
+          city: fields[2],
+          neighborhood: fields[3],
+          street: fields[4].replace("\r", ""),
+        };
+      }
+    }
+
+    return "NOT_FOUND";
+  } catch (error) {
+    console.error("Error reading file:", error);
+    return "FILE_NOT_FOUND";
+  }
+}
+
+module.exports = { getAddressInfoByCEP };
